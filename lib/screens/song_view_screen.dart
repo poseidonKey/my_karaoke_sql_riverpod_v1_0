7dbd694917cb111e17690e9c7e5629fad45699af
poseidon <jsy10303@gmail.com>
2024-02-25 10:18:16 +0900

    view 수정 함.

--- lib/screens/song_view_screen.dart
+++ lib/screens/song_view_screen.dart
@@ -11,35 +11,87 @@ class SongViewscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '곡 상세정보',
-      body: Column(
-        children: [
-          Text('제목 : ${item.songName}'),
-          Text('장르 : ${item.songJanre}'),
-          Text('즐겨찾기 : ${item.songFavorite}'),
-          Text('금영노래방 : ${item.songGYNumber}'),
-          Text('태진노래방 : ${item.songTJNumber}'),
-          Text('기록날짜 : ${item.songCreateTime}'),
-          Text('특기사항 : ${item.songETC}'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('유튜브 보기 '),
-              // Text('보기 : ${item.songUtubeAddress}'),
-              IconButton(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: Column(
+          children: [
+            _getContentsValue(subject: '제목 : ', content: item.songName),
+            _getContentsValue(
+                subject: '장르 : ',
+                content: item.songJanre == 'true' ? 'Check' : '추가하지 않음'),
+            _getContentsValue(subject: '즐겨찾기 : ', content: item.songFavorite),
+            _getContentsValue(subject: '금영노래방 : ', content: item.songGYNumber),
+            _getContentsValue(subject: '태진노래방 : ', content: item.songTJNumber),
+            _getContentsValue(
+                subject: '기록 날짜 : ', content: item.songCreateTime),
+            _getContentsValue(subject: '특기사항 : ', content: item.songETC),
+            SizedBox(
+                child: Container(
+              height: 5,
+              color: Colors.blueAccent,
+            )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  '유튜브 보기 :',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                IconButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) {
+                          return const YoutubeViewScreen(
+                            songUrl:
+                                'https://www.youtube.com/watch?v=S6y2S3tfMRQ',
+                          );
+                          // return const CustomYoutubePlayer();
+                        }),
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.movie,
+                      size: 40,
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: 40,
+              width: double.infinity,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.orange),
                   onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) {
-                        return const YoutubeViewScreen(
-                          songUrl:
-                              'https://www.youtube.com/watch?v=S6y2S3tfMRQ',
-                        );
-                        // return const CustomYoutubePlayer();
-                      }),
-                    );
+                    Navigator.of(context).pop();
                   },
-                  icon: const Icon(Icons.view_agenda))
-            ],
+                  child: const Text('back')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getContentsValue({required String subject, required String content}) {
+    const subjectStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
+    const contentStyle =
+        TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.red);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          Text(
+            subject,
+            style: subjectStyle,
           ),
+          const SizedBox(
+            width: 20,
+          ),
+          Text(content, style: contentStyle),
         ],
       ),
     );
